85b65c3a8f91a6b36b9d3bda5284f45281a66d5f
imkariya <mkariya@deqode.com>
2022-03-28 17:42:26 +0530

    ok

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const serverless = require('serverless-http')
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
     res.send('Hello World')
 })
 
